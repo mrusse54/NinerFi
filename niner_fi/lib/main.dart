0684ee5dc709b01db2b817458ed8eb2d91195a1e
mrusse54 <79732557+mrusse54@users.noreply.github.com>
2022-10-20 21:54:06 -0400

    Example pull
    Example Pull

--- niner_fi/lib/main.dart
+++ niner_fi/lib/main.dart
@@ -30,7 +30,7 @@ class _MyAppState extends State<MyApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('DummyName'),
+          title: const Text('NinerFi'),
           backgroundColor: Colors.green[700],
         ),
         drawer: const NavigationDrawer(),
